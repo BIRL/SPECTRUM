644b6df62f3153058f91d31ba85b77280b8e0b0d
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-25 06:34:04 +0500

    Update Prep_Score_PSTs.m

--- ToolBox/PST/Prep_Score_PSTs.m
+++ ToolBox/PST/Prep_Score_PSTs.m
@@ -20,119 +20,77 @@ Tag_ladder = Extract_PSTs(prot_ExperimentalPeakList,User_Taglength_max_threshold
 if ~isempty(Tag_ladder)
     Tag_Ladder = {};
     for l = 1:size(Tag_ladder.Taglength,2)
-        Tag_Ladder{l} = {cell2mat(Tag_ladder.Tag(l)),Tag_ladder.Taglength(l),Tag_ladder.Score_Error(l),Tag_ladder.Frequency(l)}; %#ok<*AGROW>
+        Tag_Ladder{l} = {cell2mat(Tag_ladder.Tag(l)),Tag_ladder.Errors(l),...
+            Tag_ladder.Taglength(l),Tag_ladder.Tags_Error_Array(l),...
+            Tag_ladder.Tags_Intesnsity_Array(l)}; %#ok<*AGROW>
     end
 else
     Tag_Ladder = {};
 end
 
+%% Accomodate Isoforms %%Updated 20210325
 if not(isempty(Tag_Ladder))
     Temp_Ladder = {};
     Hop_threshold=getappdata(0,'Hop_threshold');
     
+    User_Hop_threshold = double(string(Hop_threshold));
+    
     %% PST correction for I,L,Q and K
-    j=0;
-    for i=1:length( Tag_Ladder)
-        ind_se=strfind(Tag_Ladder{i}{1},'L');
-        if not(isempty(ind_se))
-            j=j+1;
-            tem_seq=Tag_Ladder{i}{1};
-            for ind=1:length(ind_se)
-                tem_seq(ind_se(ind))='I';
-                Temp_Ladder{j}{1}=tem_seq;
-                Temp_Ladder{j}{2}=Tag_Ladder{i}{2};
-                Temp_Ladder{j}{3}=Tag_Ladder{i}{3};
-                Temp_Ladder{j}{4}=Tag_Ladder{i}{4};
-                j=j+1;
-            end
-        end
-        
-        %%%
-        ind_se=strfind(Tag_Ladder{i}{1},'D');
-        if not(isempty(ind_se))
-            j=j+1;
-            tem_seq=Tag_Ladder{i}{1};
-            for ind=1:length(ind_se)
-                tem_seq(ind_se(ind))='B';
-                Temp_Ladder{j}{1}=tem_seq;
-                Temp_Ladder{j}{2}=Tag_Ladder{i}{2};
-                Temp_Ladder{j}{3}=Tag_Ladder{i}{3};
-                Temp_Ladder{j}{4}=Tag_Ladder{i}{4};
-                j=j+1;
-            end
-        end
-        
-        %%%
-        ind_se=strfind(Tag_Ladder{i}{1},'N');
-        if not(isempty(ind_se))
-            j=j+1;
-            tem_seq=Tag_Ladder{i}{1};
-            for ind=1:length(ind_se)
-                tem_seq(ind_se(ind))='B';
-                Temp_Ladder{j}{1}=tem_seq;
-                Temp_Ladder{j}{2}=Tag_Ladder{i}{2};
-                Temp_Ladder{j}{3}=Tag_Ladder{i}{3};
-                Temp_Ladder{j}{4}=Tag_Ladder{i}{4};
-                j=j+1;
-                
-            end
-        end
-        
-        %%%
-        ind_se=strfind(Tag_Ladder{i}{1},'E');
-        if not(isempty(ind_se))
-            j=j+1;
-            tem_seq=Tag_Ladder{i}{1};
-            for ind=1:length(ind_se)
-                tem_seq(ind_se(ind))='Z';
-                Temp_Ladder{j}{1}=tem_seq;
-                Temp_Ladder{j}{2}=Tag_Ladder{i}{2};
-                Temp_Ladder{j}{3}=Tag_Ladder{i}{3};
-                Temp_Ladder{j}{4}=Tag_Ladder{i}{4};
-                j=j+1;
-            end
-        end
-        
-        %%%
-        ind_se=strfind(Tag_Ladder{i}{1},'Q');
-        if not(isempty(ind_se))
-            j=j+1;
-            tem_seq=Tag_Ladder{i}{1};
-            for ind=1:length(ind_se)
-                tem_seq(ind_se(ind))='Z';
-                Temp_Ladder{j}{1}=tem_seq;
-                Temp_Ladder{j}{2}=Tag_Ladder{i}{2};
-                Temp_Ladder{j}{3}=Tag_Ladder{i}{3};
-                Temp_Ladder{j}{4}=Tag_Ladder{i}{4};
-                j=j+1;
-                
-            end
-        end
-
-        if (double(string(Hop_threshold))>1.5) %20200124 Bug Fix  %%(Hop_threshold>1.5)
-            ind_se=strfind(Tag_Ladder{i}{1},'Q');
-            if not(isempty(ind_se))
-                j=j+1;
-                tem_seq=Tag_Ladder{i}{1};
-                for ind=1:length(ind_se)
-                    tem_seq(ind_se(ind))='K';
-                    Temp_Ladder{j}{1}=tem_seq;
-                    Temp_Ladder{j}{2}=Tag_Ladder{i}{2};
-                    Temp_Ladder{j}{3}=Tag_Ladder{i}{3};
-                    Temp_Ladder{j}{4}=Tag_Ladder{i}{4};
-                    j=j+1;
+    j=1;
+    StartIndex = 1;
+    EndIndex = size(Tag_Ladder,2);
+    ResidueForReplacement = { 'L', 'D', 'N', 'E', 'Q' };
+    LoopRun = true;
+    Change = 0;  % Flag Use to break While Loop
+    while LoopRun
+        for i = StartIndex: EndIndex
+            Change = 0;
+            StartindexResidueAA = 1;
+            for indexResidueAA = StartindexResidueAA:size(ResidueForReplacement,2)
+                BeforeAccomodatePst = Tag_Ladder{i}{1};
+                OldResidue = ResidueForReplacement{indexResidueAA};
+                CheckResidue = strfind(BeforeAccomodatePst,OldResidue);
+                if size(CheckResidue,1) ~= 0
+                    if (OldResidue == 'L')    %%Here I think Switch Case will be more better....!!!!
+                        newResidue = "I";
+                    elseif (OldResidue == 'D')
+                        newResidue = "B";
+                    elseif (OldResidue == 'N')
+                        newResidue = "B";
+                    elseif (OldResidue == 'E')
+                        newResidue = "Z";
+                    elseif (OldResidue == 'Q' && User_Hop_threshold <= 1.5)
+                        newResidue = "Z";
+                    elseif (OldResidue == 'Q' && User_Hop_threshold > 1.5)
+                        newResidue = "K";
+                    end
+                    for iter = 1: size(BeforeAccomodatePst,2)
+                        if BeforeAccomodatePst(1, iter) == OldResidue
+                            ResidueInserted = BeforeAccomodatePst;
+                            ResidueInserted(1, iter) = newResidue;
+                            
+                            l = length(Tag_Ladder);
+                            l = l + 1;
+                            Tag_Ladder{l}{1}=ResidueInserted;
+                            Tag_Ladder{l}{2}=Tag_Ladder{i}{2};
+                            Tag_Ladder{l}{3}=Tag_Ladder{i}{3};
+                            Tag_Ladder{l}{4}=Tag_Ladder{i}{4};
+                            Tag_Ladder{l}{5}=Tag_Ladder{i}{5};
+                            Change = 1;
+                            BeforeAccomodatePst = ResidueInserted;
+                        end
+                    end
                 end
             end
         end
+        StartIndex =  EndIndex + 1;
+        EndIndex = size(Tag_Ladder, 2);
+        
+        if (StartIndex > EndIndex && Change == 0)
+            LoopRun = false;
+            break;
+        end
     end
     
-    Temp_Ladder = Temp_Ladder(~cellfun('isempty',Temp_Ladder)); % trim the ladder cell array
-    l=length(Tag_Ladder);
-    for i=1:length(Temp_Ladder)
-        l=l+1;
-        Tag_Ladder{l}{1}=Temp_Ladder{i}{1};
-        Tag_Ladder{l}{2}=Temp_Ladder{i}{2};
-        Tag_Ladder{l}{3}=Temp_Ladder{i}{3};
-        Tag_Ladder{l}{4}=Temp_Ladder{i}{4};
-    end
+    Tag_Ladder = FindUniquePstAgain( Tag_Ladder, User_tagError_threshold );
 end
